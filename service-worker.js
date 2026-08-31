eb406391cb2c85ee073fc6c48ea921ae6fffac95
jamesmethvin74 <james.methvin74@gmail.com>
2026-08-31 11:00:35 -0500

    Cache v43 Android launch icon

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "localbleachersar-shell-v42";
+const CACHE_NAME = "localbleachersar-shell-v43";
 const CORE_ASSETS = [
   "./",
   "./index.html",
@@ -17,6 +17,7 @@ const CORE_ASSETS = [
   "./reference-layout.js",
   "./team-detail.js",
   "./live-data.js",
+  "./assets/app-icon-launch-v43.svg",
   "./assets/app-icon-192-v35.png",
   "./assets/app-icon-512-v35.png",
   "./assets/splash-logo-v35.webp"
