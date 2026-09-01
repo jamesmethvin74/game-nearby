2783107c5a47feeef10fbd152dfc4836013ab298
jamesmethvin74 <james.methvin74@gmail.com>
2026-09-01 15:35:46 -0500

    Cache record recovery bridge

--- service-worker.js
+++ service-worker.js
@@ -19,6 +19,7 @@ const CORE_ASSETS = [
   "./reference-layout.js",
   "./team-detail.js",
   "./live-data.js",
+  "./record-recovery.js",
   "./school-logo-ui.js",
   "./standings.js",
   "./assets/app-icon-192-v35.png",
