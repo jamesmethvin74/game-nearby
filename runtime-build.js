a8b9f370d65da257bc621f8befd4ed3bbc8b83b4
jamesmethvin74 <james.methvin74@gmail.com>
2026-09-18 17:33:55 -0500

    M15: add factual client presentation contract

---
+++ runtime-build.js
@@ -0,0 +1,9 @@
+window.LocalBleachersRuntime = Object.freeze({
+  releaseGeneration: "m15-presentation-v71",
+  clientGeneration: 71,
+  shellGeneration: 71,
+  scheduleCacheSchema: 5,
+  standingsAssetGeneration: 71,
+  presentationContract: "display-v1",
+  clientSource: "github-pages-main"
+});
